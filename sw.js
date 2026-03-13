940c8cfd65e12ec413904b56c73dd72f0d5d958b
appsoses-creator <appsoses@gmail.com>
2026-03-13 15:59:26 -0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = "dpie-camaras-v1";
+
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/admin.html",
+  "/calculadora.html",
+  "/login.html",
+  "/relevamiento.html"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
